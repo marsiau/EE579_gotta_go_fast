29261d4bf65a0901edd3ac423c5b4a77be6219c2
DanielN08 <d.nadejde@yahoo.com>
2019-05-08 20:51:59 +0100

    Fixed the steering calibration! V2
    uploaded wrong files in the previous version

--- Firmware/sanic/PWMsetup.h
+++ Firmware/sanic/PWMsetup.h
@@ -1,16 +1,26 @@
 #include "io430.h"
 
-typedef int bool;
-#define true 1
-#define false 0
+enum FwdRwd_flag {Stop,Forward,Reverse};
+enum LR_flag {Neutral,Right,Left};
 
+// Movement flags used to determine the status of the car
+extern enum FwdRwd_flag drive_flag;
+extern enum LR_flag steer_flag;
 
-//extern int Cycles;
+// Global variables where the cycle limit is stored
+extern int FwdRwdCyclesLimit;
+extern int RLCyclesLimit;
+
+// Global variables where the current cycle is stored
+extern int FwdRwdCycle;
+extern int RLCycle;
+
 void ACLKClockSetup();
 void PWM_PeriodSetup(int Period);
 void PWM_TimerSetup();
-bool MoveFWD(int DutyCycle, int Cycles, int CyclesLimit);
-bool MoveRWD(int DutyCycle, int Cycles, int CyclesLimit);
-bool MoveLeft(int Cycles, int CyclesLimit);
-bool MoveRight(int Cycles, int CyclesLimit);
-void Stop();
+// CyclesLimit defines the movement duration
+void Drive_FWD(int DutyCycle, int CyclesLimit); 
+void Drive_RWD(int DutyCycle, int CyclesLimit);
+void Steer_Left(int CyclesLimit);
+void Steer_Right(int CyclesLimit);
+void StopCar();
